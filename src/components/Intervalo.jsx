53bf2868d6580b8030864e2d67dab35ed1937436
GFatigate <51101844+GFatigate@users.noreply.github.com>
2021-02-03 19:19:01 -0300

    action finalizado

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -1,7 +1,7 @@
 import './Intervalo.css'
 import React from 'react'
 import {connect} from 'react-redux'
-import {alterarNumeroMinimo} from '../store/actions/numeros'
+import {alterarNumeroMinimo, alterarNumeroMaximo} from '../store/actions/numeros'
 import Card from './Cards'
 
 function Intervalo(props) {
@@ -11,12 +11,12 @@ function Intervalo(props) {
             <div className="Intervalo">
                 <span>
                     <strong>Minimo: </strong>
-                    <input type="number" value={min} readOnly/>
+                    <input type="number" value={min} onChange={e => props.alteraMin(+e.target.value)}/>
                 </span>
                 
                 <span>
                     <strong>Maximo: </strong>
-                    <input type="number" value={max} readOnly/>
+                    <input type="number" value={max} onChange={e => props.alteraMax(+e.target.value)}/>
                 </span>
             </div>
         </Card>
@@ -30,13 +30,17 @@ function mapStateToProps(state){
     }
 }
 
-function mapActionCreatorsToProps(dispatch){
+function mapDispatchToProps(dispatch){
     return{
         alteraMin(novoNumero){
             const action = alterarNumeroMinimo(novoNumero)
+            dispatch(action)
+        },
+        alteraMax(novoNumero){
+            const action = alterarNumeroMaximo(novoNumero)
             dispatch(action)
         }
     }
 }
 
-export default connect(mapStateToProps, mapActionCreatorsToProps)(Intervalo)
+export default connect(mapStateToProps, mapDispatchToProps)(Intervalo)
